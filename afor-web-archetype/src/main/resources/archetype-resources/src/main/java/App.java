7bb42819fd52d6829e8314a2326ac63141cd0eb2
Matt Belcher <kiwiit@gmail.com>
2015-09-30 23:12:12 +1300

    Added maven parent project

---
+++ afor-web-archetype/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $nz.co.afor.archetype;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
