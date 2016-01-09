545f578e8d725d1b155eb5196eef62648f4c77e6
Matt Belcher <kiwiit@gmail.com>
2016-01-09 22:14:36 +1300

    Updated dependency versions

---
+++ afor-web/src/test/java/RunTest.java
@@ -0,0 +1,13 @@
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * Created by Matt Belcher on 12/10/2015.
+ */
+@RunWith(Cucumber.class)
+@ContextConfiguration("classpath:cucumber.xml")
+@CucumberOptions(format = {"pretty", "html:target/cucumber"})
+public class RunTest {
+}
