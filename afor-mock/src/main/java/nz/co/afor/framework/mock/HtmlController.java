1b6be4f5ef3cb5e554a0e659009998f8a9f0b4ca
Matt Belcher <kiwiit@gmail.com>
2016-01-20 23:13:46 +1300

    Added json responses to the mock service

--- afor-mock/src/main/java/nz/co/afor/framework/mock/HtmlController.java
+++ afor-mock/src/main/java/nz/co/afor/framework/mock/HtmlController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Matt Belcher on 18/08/2015.
@@ -13,18 +14,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HtmlController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/plainText")
-    public String plainText() {
+    public @ResponseBody String plainText() {
         return "plainText";
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/plainTextWithParameter")
-    public String plainTextWithParameter(@RequestParam(value = "parameter", required = false, defaultValue = "default parameter") String parameter, Model model) {
+    public @ResponseBody String plainTextWithParameter(@RequestParam(value = "parameter", required = false, defaultValue = "default parameter") String parameter, Model model) {
         model.addAttribute("parameter", parameter);
         return "plainTextWithParameter";
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/plainText")
-    public String plainTextPost() {
+    public @ResponseBody String plainTextPost() {
         return "plainText";
     }
 }
