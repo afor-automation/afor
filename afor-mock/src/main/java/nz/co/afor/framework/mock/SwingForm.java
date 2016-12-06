26ba339c8c8c039416bc13f34cb7d55ef4ee98c6
Matt Belcher <matt.belcher@afor.co.nz>
2016-12-06 20:44:56 +1300

    Removed form designer dependencies

--- afor-mock/src/main/java/nz/co/afor/framework/mock/SwingForm.java
+++ afor-mock/src/main/java/nz/co/afor/framework/mock/SwingForm.java
@@ -1,29 +1,40 @@
 package nz.co.afor.framework.mock;
 
-import org.springframework.stereotype.Component;
-
 import javax.swing.*;
+import java.awt.*;
 import java.util.Arrays;
 
 /**
- * Created by Matt on 24/11/2016.
+ * Created by Matt on 6/12/2016.
  */
-@Component
 public class SwingForm {
-    private JButton createUserButton;
-    private JTextField usernameTextField;
-    private JPasswordField passwordField;
-    private JList userType;
-    private JComboBox domain;
-    private JTable roles;
-    private JPanel panel;
+    private JButton createUserButton = new JButton("Create User");
+    private JTextField usernameTextField = new JTextField();
+    private JPasswordField passwordField = new JPasswordField();
+    private JList userType = new JList();
+    private JComboBox domain = new JComboBox();
+    private JTable roles = new JTable();
+    private JPanel panel = new JPanel();
+    private JPanel userPanel = new JPanel();
+    private JPanel buttonPanel = new JPanel();
 
     public JPanel getPanel() {
         return panel;
     }
 
     public SwingForm() {
+        panel.setLayout(new BorderLayout());
+        userPanel.setLayout(new BorderLayout());
+        buttonPanel.setLayout(new BorderLayout());
+        panel.add(userPanel, BorderLayout.NORTH);
+        panel.add(buttonPanel, BorderLayout.SOUTH);
+        userPanel.add(usernameTextField, BorderLayout.NORTH);
+        userPanel.add(passwordField, BorderLayout.SOUTH);
+        buttonPanel.add(createUserButton);
         createUserButton.setActionCommand("create");
+        usernameTextField.setPreferredSize(new Dimension(250,25));
+        passwordField.setPreferredSize(new Dimension(250,25));
+        createUserButton.setPreferredSize(new Dimension(250,25));
         createUserButton.addActionListener(e -> {
             if (usernameTextField.getText().equalsIgnoreCase("username") && Arrays.equals(passwordField.getPassword(), "password".toCharArray())) {
                 usernameTextField.setEnabled(false);
