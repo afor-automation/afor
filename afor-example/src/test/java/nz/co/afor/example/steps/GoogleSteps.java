e20c261cfb373caae9f4490344b18d1ef5c72749
DelphyM <dmladek@gmail.com>
2025-03-11 18:31:02 +1300

    ### New Feature for TradeMe added
    - Basic product search and filtering
    
    ### New Feature file for FlickElectric
    
    - a few scenarios to login and check account details
    - checks on the current plan
    - switch plan in progress
    
    #### Reimplemented FlickElectricSteps definition in classic way
    
    - in an attempt to persuade stubborn Cucumber, that my Scenario Outline has implemented steps
    
    ### New test for WEB slovnik.seznam.cz
    
    - added feature file
    - steps definition class
    - and the View file, which is actually not needed, as I can't see much benefits of having it.
    
    ### Hacked through Captcha
    
    - at least simple Google Search works now
    - AI Google Search could work too, but requires some API keys.

--- afor-example/src/test/java/nz/co/afor/example/steps/GoogleSteps.java
+++ afor-example/src/test/java/nz/co/afor/example/steps/GoogleSteps.java
@@ -1,6 +1,8 @@
 package nz.co.afor.example.steps;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+
 import io.cucumber.java8.En;
 import nz.co.afor.view.GoogleView;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +17,25 @@ public class GoogleSteps implements En {
     GoogleView googleView;
 
     public GoogleSteps() {
-        Given("^I am on the home page$", () -> open("/"));
-        When("^I search for \"([^\"]*)\"$", (String searchString) -> googleView.search(searchString));
-        Then("^I should see some search results$", () -> googleView.getResults().shouldBe(visible));
-        And("^the search results should contain a link with the text \"([^\"]*)\"$", (String expectedLinkText) -> googleView.getResultLinks().findBy(Condition.text(expectedLinkText)).should(exist.because(String.format("The link text '%s' should exist in the results", expectedLinkText))));
+        Given("^I am on the home page$", () -> {
+            open("/");
+            Selenide.sleep((long) (Math.random() * 3000 + 1000)); // Add delay
+        });
+
+        When("^I search for \"([^\"]*)\"$", (String searchString) -> {
+            googleView.search(searchString);
+            Selenide.sleep((long) (Math.random() * 3000 + 1000)); // Add delay
+        });
+
+        Then("^I should see some search results$", () -> {
+            googleView.solveCaptcha();
+            googleView.getResults().shouldBe(visible);
+        });
+
+        And("^the search results should contain a link with the text \"([^\"]*)\"$", (String expectedLinkText) -> {
+            googleView.getResultLinks()
+                    .findBy(Condition.text(expectedLinkText))
+                    .should(exist.because(String.format("The link text '%s' should exist in the results", expectedLinkText)));
+        });
     }
 }
